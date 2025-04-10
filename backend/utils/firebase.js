de25c97cc9d1cfc8c6debac12ea718d2ae35d998
Saloni <saloni45055@gmail.com>
2025-04-10 16:15:57 +0530

    a

--- backend/utils/firebase.js
+++
@@ -1,7 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../config/surplussmile-158af-firebase-adminsdk-fbsvc-b9a2481100.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
